985834e1af21a968c6625e5d96211ced8260cc66
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2022-10-14 17:54:55 +0100

    started the project

---
+++ e-commerce/src/App.js
@@ -0,0 +1,16 @@
+import Navbar from "./Componets/Navbar";
+import ItemsComponent from "./Componets/ItemsComponent";
+import Footer from "./Componets/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <ItemsComponent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ e-commerce/src/Componets/ItemsComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ItemsComponent() {
+  return <div>ItemsComponent</div>;
+}
+
+export default ItemsComponent;
