0778b141d4e69515ffbb79f031fc3f8b9ccdc6a9
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2022-10-26 22:07:55 +0100

    signup form is reponsive

--- e-commerce/src/Componets/SignUpForm.js
+++ e-commerce/src/Componets/SignUpForm.js
@@ -3,70 +3,19 @@ import "./styles/SignUp.css";
 
 function SignUpForm() {
   return (
-    <div className="form">
-      <div className="form-body">
-        <div className="username">
-          <label className="form__label" for="firstName">
-            First Name
-          </label>
-          <input
-            className="form__input"
-            type="text"
-            id="firstName"
-            placeholder="First Name"
-          />
+    <div className="form-container">
+      <h1>Sign Up</h1>
+      <form className="form">
+        <div className="inputs">
+          <form>
+            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="Last Name" />
+            <input type="text" placeholder="Email" />
+            <input type="text" placeholder="Password" />
+            <input type="submit" className="btn-light btn" />
+          </form>
         </div>
-        <div className="lastname">
-          <label className="form__label" for="lastName">
-            Last Name{" "}
-          </label>
-          <input
-            type="text"
-            name=""
-            id="lastName"
-            className="form__input"
-            placeholder="LastName"
-          />
-        </div>
-        <div className="email">
-          <label className="form__label" for="email">
-            Email{" "}
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="form__input"
-            placeholder="Email"
-          />
-        </div>
-        <div className="password">
-          <label className="form__label" for="password">
-            Password{" "}
-          </label>
-          <input
-            className="form__input"
-            type="password"
-            id="password"
-            placeholder="Password"
-          />
-        </div>
-        <div className="confirm-password">
-          <label className="form__label" for="confirmPassword">
-            Confirm Password{" "}
-          </label>
-          <input
-            className="form__input"
-            type="password"
-            id="confirmPassword"
-            placeholder="Confirm Password"
-          />
-        </div>
-      </div>
-      <div class="footer">
-        <button type="submit" class="btn">
-          Register
-        </button>
-      </div>
+      </form>
     </div>
   );
 }
