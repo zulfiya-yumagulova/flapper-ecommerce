607f931c7f3b49176b981d7799b63b4fa959cff4
zulfiya-yumagulova <83015952+zulfiya-yumagulova@users.noreply.github.com>
2022-12-16 11:57:46 +0000

    Update App.js
    Clear

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -10,12 +10,7 @@ import Footer from "./Componets/Footer";
 import store from "./redux/store";
 import "./App.css";
 
-/* This app has one bug, when user is on page localhost:3000/id prss the button Add 
-one time it adds the item and on the basket icon appears the digit 1, but if hte user adds the same
-item again the digit still will be 1, if the user will go to the basket page, there will be qty of item 2
-if the user adds another item the digit updates to 2. I don't know how to fix that bug and I decided to leave it like that
-because I didn't want to cheat and ask someone more experienced to help me. I used my own skills to be honest and show what can I do.
-*/
+
 
 function App() {
   return (
